f8eafcd5840dd085d1bb9c3dc5a2d780de09ed8d
Sahil <sahilkh9087@gmail.com>
2025-07-30 02:32:01 +0530

    add my site preview

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,16 +1,16 @@
 import { Container } from "@/components/container";
 import { IconBrandGithub } from "@tabler/icons-react";
+import { LinkPreview } from "../ui/link-preview";
 
 export function Footer() {
 	return (
 		<Container className="bg-neutral-50 dark:bg-neutral-900 dark:border-neutral-600 border border-neutral-200">
 			<footer className="py-2 px-6 flex justify-between">
-				<p>Portfolio inspired by {" "}
-					<a className="text-rose-500"
-						href="https://sahilkhan.site">
+				<div>Portfolio inspired by {" "}
+					<LinkPreview url="https://www.sahilkhan.site" className="font-bold text-rose-500 dark:text-rose-500">
 						@Sahil Khan
-					</a>
-				</p>
+					</LinkPreview>
+				</div>
 				<div>
 					<a href="https://github.com/Itxsahil/nextjs-portfolio-template-with-mdx">
 						<IconBrandGithub height={24} width={24} className="text-rose-500" />
